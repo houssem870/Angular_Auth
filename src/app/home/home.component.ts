d1d00fd741857dbd31c35a6ed3a9a6a58ea5d233
hou ssem <houssem.abassno@gmail.com>
2021-07-31 14:40:40 +0100

    Fifth commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+
+import {TokenService} from '../Token/Token.service'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  info: any;
+
+  constructor(private token: TokenService) {
+  }
+
+  ngOnInit() {
+    this.info = {
+      token: this.token.getToken(),
+      username: this.token.getUsername(),
+      authorities: this.token.getAuthorities()
+    };
+  }
+
+  logout() {
+    this.token.signOut();
+    window.location.reload();
+  }
+}
